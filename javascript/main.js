8ead609cde9c2c87c021486d6bd8cd4370af7546
Mikael Soares <mikael.soares@asaas.com.br>
2022-08-18 22:12:43 -0300

    desenvolvimento de aplicação de gerador de qrcode

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+const data = document.querySelector("#data");
+const qr = document.querySelector("#qr");
+
+data.addEventListener("keyup" , e =>{
+    if(e.code === "Enter"){
+    qr.innerHTML = "";
+    const href = data.value;
+    const size = 360;
+
+        new QRCode(qr , {
+            text: href,
+            width: size,
+            height: size,
+
+            colorDark: "#040404",
+            colorLight: "#e9eef4"
+        })
+    }
+})
